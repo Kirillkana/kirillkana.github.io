2daecdd78145e261403f77b755e336efc86896c5
Kirillkana <40937854+Kirillkana@users.noreply.github.com>
2020-02-13 23:22:29 +0600

    Delete curl.php

--- curl.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	curl - X POST \ 
-  https : //api.instagram.com/oauth/access_token \ - F client_id = 208855683625733 \
-   - F client_secret = f3069a294305d27589c45e90a940d618 \
-   - F grant_type = authorization_code \
-   - F redirect_uri = https://kirillkana.github.io/ \
-   - F code = AQBwvkz1GtlWc-dKcsQJJg3qN-u_DH676Rkfw9B9eKIT3G2wPzewcS3OMe5tSFqOZ-gEdcJUrUyHaJaE4DGg3vIBw0dTw4k3ivPwBvt9TkXji2opPnrSFhElXN6LtPaBgDNeF4ndMnBfdSjel8SQxWDzVaCOgxhdJcDZpndd8nCpcGT5hLmk1UdCjIqfCbRAXIpbjKUOty1ql8LTkErHnlURjuyb-phmnTjAnv9P4EcEMw
- ?>
